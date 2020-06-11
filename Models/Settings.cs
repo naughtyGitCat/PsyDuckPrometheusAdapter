1ae3d2ee7c38ea22561fb9652a2e58d015f2a6cc
psyduck007 <psyduck007@outlook.com>
2020-06-11 09:47:06 +0800

    0.1.2

---
+++ Models/Settings.cs
@@ -0,0 +1,19 @@
+﻿// ////////////////////////////////////////////////////
+// StartTime:      2020/6/10 23:05
+// FileName:       Settings.cs
+// Author:           psyduck007@outlook.com
+// Purpose:         Lazy dog does not write purpose
+// TODO:
+// ////////////////////////////////////////////////////
+//
+//
+using System;
+namespace DBAPrometheusAdapter.Models
+{
+    public class Settings
+    {
+        public Settings()
+        {
+        }
+    }
+}
